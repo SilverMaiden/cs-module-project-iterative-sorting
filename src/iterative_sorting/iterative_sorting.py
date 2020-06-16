1d51d594735380bb464b4f478fc89c1d8bbb8dd0
SilverMaiden <h.specials@gmail.com>
2020-06-16 13:34:42 -0700

    Finished but rewriting binary iterative search

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,4 @@
+from binary_search_tree import BSTNode
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -7,20 +8,34 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        while cur_index <= len(arr) - 1:
+            if arr[cur_index] < arr[smallest_index]:
+                smallest_index = cur_index
+            cur_index += 1
         # TO-DO: swap
         # Your code here
-
+        arr[i], arr[smallest_index]= arr[smallest_index], arr[i]
     return arr
 
+num_array = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+print(selection_sort(num_array))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
 
+    for i in range(0, len(arr) - 2):
+        cur_index = i
+        next_index = i+1
+        while cur_index < len(arr) - 1:
+            if arr[cur_index] > arr[next_index]:
+                arr[next_index], arr[cur_index] = arr[cur_index], arr[next_index]
+            cur_index += 1
 
+    print(arr)
     return arr
+
+bubble_sort(num_array)
 
 '''
 STRETCH: implement the Count Sort function below
@@ -31,11 +46,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +10,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    high = len(arr) - 1
+    low = 0
+    mid = high - int(high / 2)
+    arr.sort()
+    print(arr[mid])
 
-    # Your code here
 
+    if target == arr[mid]:
+        return mid
+    elif target > arr[mid]:
+        low = mid
+    elif target < arr[mid]:
+        high = mid
 
     return -1  # not found
+
